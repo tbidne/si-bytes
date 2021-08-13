ea91f5bd02ae09144479118e472a773933a15e47
Tommy Bidne <tbidne@protonmail.com>
2021-08-13 23:52:21 +1200

    Add common LiftBase class for liftAN functions
    Also add Conversion typeclass for converting between units.

--- src/ByteTypes/Data/Size.hs
+++ src/ByteTypes/Data/Size.hs
@@ -1,11 +1,21 @@
 -- | Provides the 'ByteSize' type and typeclasses for converting
 -- between units.
 module ByteTypes.Data.Size
-  ( ByteSize (..),
+  ( -- * ByteSize Tags
+    ByteSize (..),
+
+    -- * Typeclasses for converting units
+    Normalize (..),
     DecByteSize (..),
     IncByteSize (..),
+    Conversion (..),
+
+    -- * Type Families for Relating Tags
     NextUnit,
     PrevUnit,
+
+    -- * Functions
+    convert,
   )
 where
 
@@ -39,6 +49,15 @@ type family PrevUnit a where
   PrevUnit 'TB = 'GB
   PrevUnit 'PB = 'TB
 
+-- | Used for normalizing bytes @b@ such that,
+--
+-- \[
+--    1 \le \text{normalize}(b) < 1000 \iff 1\text{ B} \le b < 1000\text{ PB}
+-- \]
+class Normalize a where
+  type Result a
+  normalize :: a -> Result a
+
 -- | Typeclass for decreasing bytes to the previous units.
 class DecByteSize a where
   -- | Prev should involve 'PrevUnit'.
@@ -52,3 +71,62 @@ class IncByteSize a where
   type Next a
 
   next :: a -> Next a
+
+-- | Provides a common interface for converting between byte sizes.
+class Conversion a where
+  type BType a
+  type KType a
+  type MType a
+  type GType a
+  type TType a
+  type PType a
+  toB :: a -> BType a
+  toKB :: a -> KType a
+  toMB :: a -> MType a
+  toGB :: a -> GType a
+  toTB :: a -> TType a
+  toPB :: a -> PType a
+
+-- | Low level function for converting a numeric literal between
+-- byte sizes. @convert b1 b2@ converts /from/ @b1@ /to/ @b2@,
+-- e.g. @convert GB KB = \n -> n * 1_000_000@. The higher level
+-- byte types and functions should be preferred
+-- (e.g. 'ByteTypes.Data.Bytes', 'Normalize'), but this is here
+-- when it is needed.
+convert :: Fractional n => ByteSize -> ByteSize -> n -> n
+convert B B n = n
+convert B KB n = n / 1_000
+convert B MB n = n / 1_000_000
+convert B GB n = n / 1_000_000_000
+convert B TB n = n / 1_000_000_000_000
+convert B PB n = n / 1_000_000_000_000_000
+convert KB B n = n * 1_000
+convert KB KB n = n
+convert KB MB n = n / 1_000
+convert KB GB n = n / 1_000_000
+convert KB TB n = n / 1_000_000_000
+convert KB PB n = n / 1_000_000_000_000
+convert MB B n = n * 1_000_000
+convert MB KB n = n * 1_000
+convert MB MB n = n
+convert MB GB n = n / 1_000
+convert MB TB n = n / 1_000_000
+convert MB PB n = n / 1_000_000_000
+convert GB B n = n * 1_000_000_000
+convert GB KB n = n * 1_000_000
+convert GB MB n = n * 1_000
+convert GB GB n = n
+convert GB TB n = n / 1_000
+convert GB PB n = n / 1_000_000
+convert TB B n = n * 1_000_000_000_000
+convert TB KB n = n * 1_000_000_000
+convert TB MB n = n * 1_000_000
+convert TB GB n = n * 1_000
+convert TB TB n = n
+convert TB PB n = n / 1_000
+convert PB B n = n * 1_000_000_000_000_000
+convert PB KB n = n * 1_000_000_000_000
+convert PB MB n = n * 1_000_000_000
+convert PB GB n = n * 1_000_000
+convert PB TB n = n * 1_000
+convert PB PB n = n
